9330e0ec33ddf334cdff8a726bf05cf144c5f550
Higon Cesar <higoncesar@gmail.com>
2019-04-11 01:32:51 -0300

    Modificação no CORS

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const path      = require('path');
 const cors      = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
